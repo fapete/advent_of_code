0e00783ec387791965be6522ef691ee1eb765dd9
Fabian Peternek <f.peternek@fapet.de>
2020-12-23 21:12:42 +0100

    Day 23 solution

--- 2020/day23/src/main.rs
+++ 2020/day23/src/main.rs
@@ -9,116 +9,82 @@ fn main() {
 }
 
 struct CupGame {
-    cups: Vec<u64>,
+    cups: Vec<usize>,
     cur: usize,
-    dest: usize,
     num_cups: usize,
 }
 
 impl CupGame {
     fn from(input: &str, num_cups: usize) -> CupGame {
-        let mut cups: Vec<_> = input
+        let mut cups = vec![0; num_cups + 1];
+
+        let input: Vec<_> = input
             .trim()
             .chars()
-            .map(|x| x.to_digit(10).unwrap() as u64)
+            .map(|x| x.to_digit(10).unwrap() as usize)
             .collect();
-        for i in 10..num_cups + 1 {
-            cups.push(i as u64);
+
+        for i in 1..input.len() {
+            cups[input[i - 1]] = input[i];
+        }
+        if num_cups > 9 {
+            cups[input[8]] = 10;
+
+            for i in 11..cups.len() {
+                cups[i - 1] = i;
+            }
+            cups[num_cups] = input[0];
+        } else {
+            cups[input[8]] = input[0];
         }
 
         CupGame {
             cups,
-            cur: 0,
-            dest: 0,
+            cur: input[0],
             num_cups,
         }
     }
 
     fn move_cups(&mut self) {
         let out_cups = [
-            self.cups[(self.cur + 1) % self.num_cups],
-            self.cups[(self.cur + 2) % self.num_cups],
-            self.cups[(self.cur + 3) % self.num_cups],
+            self.cups[self.cur],
+            self.cups[self.cups[self.cur]],
+            self.cups[self.cups[self.cups[self.cur]]],
         ];
 
-        //eprintln!("---- Move ----");
-        //eprintln!("Cups: {:?}", self.cups);
-        //eprintln!("Current: {}", self.cups[self.cur]);
-        //eprintln!("Pick up: {:?}", out_cups);
-        //eprintln!("Destination: {}", self.cups[self.dest]);
-        //eprintln!("\n");
+        let new_next = self.cups[self.cups[self.cups[self.cups[self.cur]]]];
 
-        let mut cur_to_idx = (self.cur + 1) % self.num_cups;
-        let mut cur_from_idx = (self.cur + 4) % self.num_cups;
-        // Shift cups between the ones taken out and dest
-        while cur_from_idx != self.dest {
-            self.cups[cur_to_idx] = self.cups[cur_from_idx];
-            cur_to_idx = (cur_to_idx + 1) % self.num_cups;
-            cur_from_idx = (cur_from_idx + 1) % self.num_cups;
+        let mut dest = self.decrement_dest(self.cur);
+        while out_cups.contains(&dest) {
+            dest = self.decrement_dest(dest);
         }
-        // move dest cup
-        self.cups[cur_to_idx] = self.cups[cur_from_idx];
 
-        // put cups taken out back in
-        for i in 1..4 {
-            self.cups[(cur_to_idx + i) % self.num_cups] = out_cups[i - 1];
-        }
-    }
+        self.cups[out_cups[2]] = self.cups[dest];
+        self.cups[dest] = self.cups[self.cur];
+        self.cups[self.cur] = new_next;
 
-    fn find_dest(&mut self) {
-        let mut dest_value = self.decrement_dest(self.cups[self.cur]);
-        //eprintln!("Dest Value: {}", dest_value);
-        let mut dest_idx = self.find_idx_of(dest_value);
-        while self.is_move_cup(dest_idx) {
-            dest_value = self.decrement_dest(dest_value);
-            //  eprintln!("Dest Value: {}", dest_value);
-            dest_idx = self.find_idx_of(dest_value);
-        }
-        self.dest = dest_idx;
-    }
-
-    fn increment_cur(&mut self) {
-        self.cur = (self.cur + 1) % self.num_cups;
-    }
-
-    fn is_move_cup(&self, idx: usize) -> bool {
-        idx == (self.cur + 1) % self.num_cups
-            || idx == (self.cur + 2) % self.num_cups
-            || idx == (self.cur + 3) % self.num_cups
-    }
-
-    fn find_idx_of(&self, value: u64) -> usize {
-        //eprintln!("Looking for index of {}", value);
-        self.cups
-            .iter()
-            .enumerate()
-            .find(|(_, x)| **x == value)
-            .unwrap()
-            .0
+        self.cur = self.cups[self.cur];
     }
 
     fn play_for(&mut self, moves: u64) {
-        for i in 0..moves {
-            self.find_dest();
+        for _ in 0..moves {
             self.move_cups();
-            self.increment_cur();
-            if i % 500_000 == 0 {
-                println!("Move {}", i);
-            }
         }
     }
 
     fn result(&self) -> String {
         let mut result = String::new();
-        let init = self.find_idx_of(1);
-        for i in 1..self.num_cups {
-            result.push_str(format!("{}", self.cups[(init + i) % self.num_cups]).as_str());
+        let mut cur_val = self.cups[1];
+        while cur_val != 1 {
+            result.push_str(format!("{}", cur_val).as_str());
+            cur_val = self.cups[cur_val];
         }
         result
     }
-    fn decrement_dest(&self, d: u64) -> u64 {
+
+    fn decrement_dest(&self, d: usize) -> usize {
         if d - 1 == 0 {
-            self.num_cups as u64
+            self.num_cups
         } else {
             d - 1
         }
@@ -132,13 +98,12 @@ fn solve1(fname: &str, moves: u64) -> String {
     cupgame.result()
 }
 
-fn solve2(fname: &str, moves: u64, cups: usize) -> u64 {
+fn solve2(fname: &str, moves: u64, cups: usize) -> usize {
     let input = read_file(fname).unwrap();
     let mut cupgame = CupGame::from(input.as_str(), cups);
     cupgame.play_for(moves);
-    let one = cupgame.find_idx_of(1);
-    let c1 = cupgame.cups[(one + 1) % cups];
-    let c2 = cupgame.cups[(one + 2) % cups];
+    let c1 = cupgame.cups[1];
+    let c2 = cupgame.cups[c1];
     c1 * c2
 }
 
@@ -146,6 +111,12 @@ fn solve2(fname: &str, moves: u64, cups: usize) -> u64 {
 fn test_solve1() {
     assert_eq!(solve1("test", 10), String::from("92658374"));
     assert_eq!(solve1("test", 100), String::from("67384529"));
+}
+
+#[test]
+fn test_game_from() {
+    let game = CupGame::from(read_file("test").unwrap().as_str(), 12);
+    assert_eq!(game.cups, vec![0, 2, 5, 8, 6, 4, 7, 10, 9, 1, 11, 12, 3]);
 }
 
 #[test]
