b8cdc11794b5f564200698a1f9cfee2df47030e4
Fabian Peternek <f.peternek@fapet.de>
2021-12-01 14:14:41 +0100

    Part 2 solution

--- 2021/day01/day1.hs
+++ 2021/day01/day1.hs
@@ -2,10 +2,23 @@ diffs :: Num a => [a] -> [a]
 diffs (x:y:xs) = y-x:diffs (y:xs)
 diffs x = []
 
+sliding (x:y:z:xs) = x+y+z:sliding (y:z:xs)
+sliding _ = []
+
 parseInt = map (\y -> read y :: Integer)
 
-main = do
+part1 intList = length $ filter (> 0) $ diffs intList
+
+part2 intList = part1 $ sliding intList
+
+getInput = do
     x <- readFile "./input"
-    let intList = parseInt $ lines x
-    let diff = diffs intList
-    return (length (filter (> 0) diff))
+    return (parseInt $ lines x)
+
+solve1 = do
+    input <- getInput
+    return (part1 input)
+
+solve2 = do
+    input <- getInput
+    return (part2 input)
