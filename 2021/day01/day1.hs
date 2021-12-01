fc24f52d64a8ff04ce905948f898150079813e7b
Fabian Peternek <f.peternek@fapet.de>
2021-12-01 14:49:32 +0100

    Qol change to read input file from command line

--- 2021/day01/day1.hs
+++ 2021/day01/day1.hs
@@ -1,3 +1,5 @@
+import System.Environment
+
 diffs :: Num a => [a] -> [a]
 diffs (x:y:xs) = y-x:diffs (y:xs)
 diffs x = []
@@ -11,14 +13,22 @@ part1 intList = length $ filter (> 0) $ diffs intList
 
 part2 intList = part1 $ sliding intList
 
-getInput = do
-    x <- readFile "./input"
+getInput filename = do
+    x <- readFile filename
     return (parseInt $ lines x)
 
-solve1 = do
-    input <- getInput
+solve1 filename = do
+    input <- getInput filename
     return (part1 input)
 
-solve2 = do
-    input <- getInput
+solve2 filename = do
+    input <- getInput filename
     return (part2 input)
+
+main = do
+    args <- getArgs 
+    let filename = head args
+    p1Solution <- solve1 filename
+    p2Solution <- solve2 filename
+    putStrLn ("Part 1: " ++ show p1Solution)
+    putStrLn ("Part 2: " ++ show p2Solution)
