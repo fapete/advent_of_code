3763790d1734e102de389be369a054b61ebf76c5
Fabian Peternek <f.peternek@fapet.de>
2021-12-09 09:52:07 +0100

    Day 9 Part 2 Solution

--- 2021/day09/day9.hs
+++ 2021/day09/day9.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE ViewPatterns #-}
 import System.Environment
 import Data.Char
+import Data.List (sort)
+import qualified Data.Set as Set
 
 -- IO Scaffolding
 
@@ -7,9 +10,9 @@ main = do
     args <- getArgs
     let filename = head args
     p1Solution <- solve part1 filename
-    --p2Solution <- solve part2 filename
+    p2Solution <- solve part2 filename
     putStrLn ("Part 1: " ++ show p1Solution)
-    --putStrLn ("Part 2: " ++ show p2Solution)
+    putStrLn ("Part 2: " ++ show p2Solution)
 
 solve :: ([[Int]] -> Int) -> FilePath -> IO Int
 solve fn filename = do
@@ -22,13 +25,27 @@ getInput :: FilePath -> IO [[Int]]
 getInput filename = do
     x <- readFile filename
     let lineLength = length (head $ lines x) + 2
-    return $ [take lineLength [10,10..]] ++ map (padInputLine . map (read . (:[]))) (lines x) ++ [take lineLength [10,10..]]
+    return $ [take lineLength [9,9..]] ++ map (padInputLine . map (read . (:[]))) (lines x) ++ [take lineLength [9,9..]]
 
-padInputLine xs = [10] ++ xs ++ [10]
+padInputLine xs = [9] ++ xs ++ [9]
 
 -- Solution Logic
 
 part1 input = sum $ map (+1) $ [getPoint input p | p <- findLowPoints input]
+
+part2 input = product $ take 3 $ reverse $ sort $ getBasinSizes input $ findLowPoints input
+
+getBasinSizes xs = map (Set.size . findBasin xs)
+
+findBasin input (x, y) = findBasin' input (Set.singleton (x,y))
+findBasin' :: [[Int]] -> Set.Set (Int, Int) -> Set.Set (Int, Int)
+findBasin' input visited = if Set.null newPoints then visited else findBasin' input (Set.union newPoints visited)
+    where newPoints = newlyAdjacentPoints input visited
+
+newlyAdjacentPoints input visited = Set.filter (\point -> Set.notMember point visited && getPoint input point /= 9) $  foldl1 Set.union $ Set.map (getAdjacent input) visited
+
+getAdjacent :: [[Int]] -> (Int, Int) -> Set.Set (Int, Int)
+getAdjacent input (x,y) = Set.fromList [(x, y-1), (x,y+1), (x-1, y), (x+1, y)]
 
 findLowPoints xs = filter (isLow xs) ([(x,y) | x <- [1..rows], y <- [1..cols]])
     where rows = length xs - 2
