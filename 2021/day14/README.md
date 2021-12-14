d3d16db79321dfe408fd6ed2dd2b08981afbc7ca
Fabian Peternek <f.peternek@fapet.de>
2021-12-14 15:25:53 +0100

    Day 14 part 2 solution

---
+++ 2021/day14/README.md
@@ -0,0 +1,1 @@
+Solution isn't always correct: it may be off by 1 or 2 if the most frequent letters are the same as first/last letter in the initial string. Didn't happen for my input, didn't bother correcting it.
--- 2021/day14/day14.hs
+++ 2021/day14/day14.hs
@@ -4,46 +4,51 @@ import qualified Data.Bifunctor
 import Data.Maybe (mapMaybe)
 import Data.List (sort)
 
-
 -- IO Scaffolding
 
 main = do
   args <- getArgs
   let filename = head args
   p1Solution <- solve part1 filename
-  --p2Solution <- solve part2 filename
+  p2Solution <- solve part2 filename
   putStrLn ("Part 1: " ++ show p1Solution)
-  --putStrLn ("Part 2: " ++ show p2Solution)
+  putStrLn ("Part 2: " ++ show p2Solution)
 
 solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-type Dictionary = Map.Map String String
-
-getInput = fmap (Data.Bifunctor.bimap head parseRules . break (== "") . lines) . readFile
+getInput = fmap (Data.Bifunctor.bimap (pairCounts . head) parseRules . break (== "") . lines) . readFile
 
 parseRules = Map.fromList . map parseRule . tail
 
-parseRule [x, y, ' ','-','>',' ', z] = ([x, y], [x, z, y])
+parseRule [x, y, ' ','-','>',' ', z] = ([x, y], ([x,z],[z, y]))
 parseRule _ = error "invalid rule format)"
 
 -- Solution Logic
 
-part1 :: (String, Dictionary) -> Int
-part1 = (\x -> mostCommon x - leastCommon x) . (!!10) . uncurry applyRules
+part1 = (`div` 2) . (\x -> mostCommon x - leastCommon x) . (!!10) . uncurry applyRules
+
+part2 = (`div` 2) . (\x -> mostCommon x - leastCommon x) . (!!40) . uncurry applyRules
 
 mostCommon = maximum . map snd . letterCounts Map.empty
 
 leastCommon = minimum . map snd . letterCounts Map.empty
 
-letterCounts counts (x:xs) = letterCounts (Map.insertWith (+) x 1 counts) xs
-letterCounts counts [] = Map.toList counts
+letterCounts :: Map.Map String Int -> Map.Map String Int -> [(String, Int)]
+letterCounts letterCounts pairCounts = Map.toList 
+  $ foldl1 (Map.unionWith (+)) 
+  $ map (\(pair, count) -> Map.insertWith (+) [head pair] count (Map.insertWith (+) [last pair] count letterCounts)) 
+  $ Map.assocs pairCounts
 
-applyRules xs dict = xs : applyRules grownPolymer dict
-    where
-      grownPolymer = head (head triples) : foldl1 (++) (map tail triples)
-      triples = mapMaybe (`Map.lookup` dict) (getPairs xs)
+pairCounts xs = foldl1 (Map.unionWith (+)) [Map.singleton p 1 | p <- getPairs xs]
+
+applyRules counts dict = counts : applyRules (foldl1 (Map.unionWith (+)) $ getNewCounts (Map.assocs counts) dict) dict
+
+getNewCounts pairs dict = [Map.fromList [(fst (unsafeMaybe $ Map.lookup rule dict), count), (snd (unsafeMaybe $ Map.lookup rule dict), count)] | (rule, count) <- pairs ]
+
+unsafeMaybe (Just i) = i
+unsafeMaybe Nothing = error "Tried to unpack Nothing"
 
 getPairs (x:y:xs) = [x,y] : getPairs (y:xs)
 getPairs _ = []
