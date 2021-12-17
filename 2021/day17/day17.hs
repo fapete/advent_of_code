24b762ad0edfed5eb8e816e046a1ccb828340d3f
Fabian Peternek <f.peternek@fapet.de>
2021-12-17 11:49:25 +0100

    No need to compute trajectories twice

--- 2021/day17/day17.hs
+++ 2021/day17/day17.hs
@@ -8,8 +8,7 @@ import Data.Bifunctor (bimap)
 main = do
   args <- getArgs
   let filename = head args
-  p1Solution <- solve part1 filename
-  p2Solution <- solve part2 filename
+  (p1Solution, p2Solution) <- solve parts filename
   putStrLn ("Part 1: " ++ show p1Solution)
   putStrLn ("Part 2: " ++ show p2Solution)
 
@@ -31,10 +30,8 @@ parse xs = ((x1, y1), (x2, y2))
 
 -- Solution Logic
 
-part1 :: Bounds -> Int
-part1 bounds = maximum $ map snd =<< trajectoriesInBounds bounds
-
-part2 = length . trajectoriesInBounds
+parts bounds = (maximum $ map snd =<< t, length t)
+    where t = trajectoriesInBounds bounds
 
 -- Choosing 3000 for upper bound on y on the completely unfounded reasoning that it's probably high enough.
 trajectoriesInBounds bounds = filter (any (inBounds bounds)) [trajectory bounds x y (0,0) | x <- [0..(maxX bounds)], y <- [(minY bounds)..3000]]
@@ -52,4 +49,5 @@ trajectory bounds dx dy pos@(x, y)
     | overshot bounds pos = []
     | otherwise = pos : trajectory bounds (if dx > 0 then dx-1 else 0) (dy - 1) (x+dx, y + dy)
 
+overshot :: (Ord a1, Ord a2) => ((a3, a2), (a1, b)) -> (a1, a2) -> Bool
 overshot ((_, y1), (x2, _)) (x,y) = x > x2 || y < y1
