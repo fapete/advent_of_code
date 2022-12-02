a0514a5664b26add8293ad9fa9237c4b5ab51746
Fabian Peternek <f.peternek@fapet.de>
2022-12-02 18:31:22 +0100

    where I ended up

--- 2021/day22/day22.hs
+++ 2021/day22/day22.hs
@@ -1,5 +1,7 @@
+import Data.List (subsequences)
 import qualified Data.Set as Set
 import System.Environment
+import THNames (overlappableDataConKey)
 
 -- IO Scaffolding
 
@@ -15,22 +17,25 @@ solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-type Interval = (Int, Int)
-
 type Point = (Int, Int, Int)
 
-data Command = On Interval Interval Interval | Off Interval Interval Interval deriving (Show)
+type Interval = (Integer, Integer)
+
+data Command = On Cube | Off Cube deriving (Show)
+
+type Cube = (Interval, Interval, Interval)
 
 getInput = fmap (map parseLine . lines) . readFile
 
 parseLine xs = case xs of
-  ('o' : 'n' : ys) -> On xInterval yInterval zInterval
-  ('o' : 'f' : 'f' : ys) -> Off xInterval yInterval zInterval
+  ('o' : 'n' : ys) -> On cube
+  ('o' : 'f' : 'f' : ys) -> Off cube
   _ -> error "Invalid line format"
   where
-    (xInterval, remaining) = parseInterval $ dropWhile (/= ' ') xs
-    (yInterval, remaining') = parseInterval remaining
-    (zInterval, _) = parseInterval remaining'
+    cube = ((x1, x2), (y1, y2), (z1, z2))
+    ((x1, x2), remaining) = parseInterval $ dropWhile (/= ' ') xs
+    ((y1, y2), remaining') = parseInterval remaining
+    ((z1, z2), _) = parseInterval remaining'
 
 parseInterval xs = ((from, to), remaining)
   where
@@ -41,19 +46,58 @@ parseInterval xs = ((from, to), remaining)
 
 -- Solution Logic
 
-getPoints :: Interval -> Interval -> Interval -> [Point]
-getPoints xInt yInt zInt = [(x, y, z) | x <- interval xInt, y <- interval yInt, z <- interval zInt]
+getPoints ((x1, x2), (y1, y2), (z1, z2)) = [(x, y, z) | x <- [x1 .. x2], y <- [y1 .. y2], z <- [z1 .. z2]]
 
-interval (from, to) = [from .. to]
+within50 (On (i1, i2, i3)) = max50 i1 && max50 i2 && max50 i3
+within50 (Off (i1, i2, i3)) = max50 i1 && max50 i2 && max50 i3
 
-within50 (On xInt yInt zInt) = max50 xInt && max50 yInt && max50 zInt
-within50 (Off xInt yInt zInt) = max50 xInt && max50 yInt && max50 zInt
+max50 (x, y) = x >= -50 && x <= 50 && y >= -50 && y <= 50
 
-max50 (from, to) = from >= -50 && from <= 50 && to >= -50 && to <= 50
-
-applyCommand (On xInt yInt zInt) litPoints = Set.union litPoints $ Set.fromList $ getPoints xInt yInt zInt
-applyCommand (Off xInt yInt zInt) litPoints = Set.difference litPoints $ Set.fromList $ getPoints xInt yInt zInt
+applyCommand (On cube) litPoints = Set.union litPoints $ Set.fromList $ getPoints cube
+applyCommand (Off cube) litPoints = Set.difference litPoints $ Set.fromList $ getPoints cube
 
 part1 = Set.size . foldl (flip applyCommand) Set.empty . filter within50
 
-part2 xs = Set.size $ foldl (flip applyCommand) Set.empty xs
+sizeCubeUnion cube1 cube2 = cubeSize cube1 + cubeSize cube2 - cubeSize (intersectionCube cube1 cube2)
+
+union = sum . map cubeSize
+
+intersection :: [Cube] -> Integer
+intersection = cubeSize . foldl1 intersectionCube
+
+inclusionExclusion :: [Cube] -> Integer
+inclusionExclusion = foldl (\acc i -> acc + ((-1) ^ (length i + 1) * intersection i)) 0 . tail . subsequences
+
+sizeCubeDifference cube1 cube2 = cubeSize cube1 - cubeSize (intersectionCube cube1 cube2)
+
+intersectionCube (i1, i2, i3) (i1', i2', i3') = (i1 `intersect` i1', i2 `intersect` i2', i3 `intersect` i3')
+
+intersect (x1, x2) (x1', x2') = (max x1 x1', min x2 x2')
+
+cubeSize :: Cube -> Integer
+cubeSize (i1, i2, i3) = intervalSize i1 * intervalSize i2 * intervalSize i3
+
+intervalSize :: Interval -> Integer
+intervalSize (x1, x2) = x2 - x1 + 1
+
+isOn (On cube) = True
+isOn (Off cube) = False
+
+extractCube (On cube) = cube
+extractCube (Off cube) = cube
+
+allIntersections cube = map (intersectionCube cube)
+
+lights numLit processedCommands (comm@(On cube) : remainingCommands) = lights (numLit + newLights) (comm : processedCommands) remainingCommands
+  where
+    newLights = allIntersections cube (map extractCube)
+lights numLit processedCommands (comm@(Off cube) : remainingCommands) = lights (numLit - removedLights) (comm : processedCommands) remainingCommands
+lights numLit _ [] = numLit
+
+newPart1 = inclusionExclusion . map extractCube . filter (\c -> isOn c && within50 c)
+
+part1OnlyOn = Set.size . foldl (flip applyCommand) Set.empty . filter (\c -> isOn c && within50 c)
+
+part2OnlyOn = Set.size . foldl (flip applyCommand) Set.empty . filter isOn
+
+part2 xs = 4
