67fc9fc47edc6c4deedee9dcf6dd01cd22856c72
Fabian Peternek <f.peternek@fapet.de>
2022-12-04 10:56:08 +0100

    Day 4 solution

--- 2022/day04/day04.hs
+++ 2022/day04/day04.hs
@@ -7,17 +7,32 @@ main = do
   args <- getArgs
   let filename = head args
   p1Solution <- solve part1 filename
-  -- p2Solution <- solve part2 filename
+  p2Solution <- solve part2 filename
   putStrLn ("Part 1: " ++ show p1Solution)
-
--- putStrLn ("Part 2: " ++ show p2Solution)
+  putStrLn ("Part 2: " ++ show p2Solution)
 
 solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-getInput = fmap (lines) . readFile
+data Interval = Interval Integer Integer deriving (Show, Eq, Ord)
+
+getInput = fmap (map (parseIntervals . myBreak (== ',')) . lines) . readFile
+
+myBreak with xs = (fst b, tail (snd b))
+  where
+    b = break with xs
+
+parseIntervals (i1, i2) = (parseInterval i1, parseInterval i2)
+
+parseInterval s = (\(from, to) -> Interval (read from) (read to)) $ myBreak (== '-') s
 
 -- Solution Logic
 
-part1 xs = 4
+fullyContains (Interval from to) (Interval from' to') = from <= from' && to >= to'
+
+overlaps (Interval from to) (Interval from' to') = from <= from' && to >= from' || to >= to' && from <= to'
+
+part1 = length . filter (\(i1, i2) -> fullyContains i1 i2 || fullyContains i2 i1)
+
+part2 = length . filter (\(i1, i2) -> overlaps i1 i2 || overlaps i2 i1)
