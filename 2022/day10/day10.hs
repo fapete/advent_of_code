ee62349709fc3a79cfc2eddceb50600104b4a4d1
Fabian Peternek <f.peternek@fapet.de>
2022-12-10 16:12:51 +0100

    Day 10 Solution

--- 2022/day10/day10.hs
+++ 2022/day10/day10.hs
@@ -7,10 +7,10 @@ main = do
   args <- getArgs
   let filename = head args
   p1Solution <- solve part1 filename
-  -- p2Solution <- solve part2 filename
+  p2Solution <- solve part2 filename
   putStrLn ("Part 1: " ++ show p1Solution)
-
--- putStrLn ("Part 2: " ++ show p2Solution)
+  putStrLn "Part 2: "
+  putStrLn p2Solution
 
 solve fn = fmap fn . getInput
 
@@ -27,12 +27,16 @@ parseInstruction _ = error "Invalid instruction"
 
 -- Solution Logic
 
-execute (Add x) register cycle = (register, cycle + 2, register + x)
-execute Noop register cycle = (register, cycle + 1, register)
+execute (Add x) register cycle = [(register, cycle + 1, register), (register, cycle + 2, register + x)]
+execute Noop register cycle = [(register, cycle + 1, register)]
 
 relevantCycles = [20, 60 ..]
 
-executeAll = scanl (\(_, cycleAfter, after) instruction -> execute instruction after cycleAfter) (1, 0, 1)
+getCycle (_, cycle, _) = cycle
+
+getRegisterAfter (_, _, value) = value
+
+executeAll = concat . scanl (\result instruction -> execute instruction (getRegisterAfter $ last result) (getCycle $ last result)) [(1, 0, 1)]
 
 signalStrengths [] _ = []
 signalStrengths (c : cycles) executionResults = (c * valueDuringCycle) : signalStrengths cycles remainingResults
@@ -40,4 +44,18 @@ signalStrengths (c : cycles) executionResults = (c * valueDuringCycle) : signalS
     remainingResults = dropWhile (\(_, cycleAfter, _) -> cycleAfter < c) executionResults
     (valueDuringCycle, _, _) = head remainingResults
 
-part1 instructions = sum $ signalStrengths (take 6 relevantCycles) (executeAll instructions)
+getPixel (value, cycle, _)
+  | ((cycle - 1) `mod` 40) `elem` spritePositions = '#'
+  | otherwise = ' '
+  where
+    spritePositions = [value - 1, value, value + 1]
+
+drawScreen :: [(Int, Int, Int)] -> [Char]
+drawScreen = foldl (\agg executionResult -> agg ++ [getPixel executionResult]) ""
+
+splitIntoLines [] = []
+splitIntoLines line = take 40 line ++ "\n" ++ splitIntoLines (drop 40 line)
+
+part1 = sum . signalStrengths (take 6 relevantCycles) . executeAll
+
+part2 = splitIntoLines . drawScreen . tail . executeAll
