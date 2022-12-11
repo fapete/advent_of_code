296e3a2c4318dcecb029adca7988d16b55e5f8fe
Fabian Peternek <f.peternek@fapet.de>
2022-12-11 11:32:10 +0100

    Day 11 Part 1

---
+++ 2022/day11/README.md
@@ -0,0 +1,3 @@
+# Day 11
+
+Part 1 only, don't have time for part 2.
--- 2022/day11/day11.hs
+++ 2022/day11/day11.hs
@@ -1,5 +1,10 @@
+import Data.Bifunctor (second)
+import Data.Bool (bool)
 import Data.List
+import Data.List.Split (splitOn)
+import Data.Map qualified as M
 import System.Environment
+import Text.Read (readMaybe)
 
 -- IO Scaffolding
 
@@ -16,7 +21,66 @@ solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-getInput = fmap (lines) . readFile
+data Monkey = Monkey [Int] (Int -> Int) (Int -> Int) Int -- List of Items held, Worry level increase function, Throw to which monkey function, inspections
+
+getInput = fmap (M.fromList . zip [0 ..] . map parseMonkey . paragraphs) . readFile
+
+paragraphs = splitOn "\n\n"
+
+parseMonkey monkeyLines = Monkey items operation test 0
+  where
+    ls = tail $ lines monkeyLines
+    items = parseItems $ head ls
+    operation = parseOperation $ ls !! 1
+    test = parseTest $ drop 2 ls
+
+parseItems :: String -> [Int]
+parseItems line = items
+  where
+    (_, itemNums) = second (drop 2) $ break (== ':') line
+    items = map read $ splitOn "," itemNums
+
+parseOperation :: String -> (Int -> Int)
+parseOperation line
+  | head relevant == "+" = (+ (read $ last relevant))
+  | head relevant == "*" = multOperation (readMaybe $ last relevant)
+  where
+    (_, relevant) = second (splitOn " " . drop 2) $ break (== 'd') line
+    multOperation (Just i) = (* i)
+    multOperation Nothing = \x -> x * x
+
+parseTest :: [String] -> (Int -> Int)
+parseTest lines = \x -> bool falseTarget trueTarget $ x `mod` divisor == 0
+  where
+    divisor = read $ last $ splitOn " " $ head lines
+    trueTarget = read $ last $ splitOn " " $ lines !! 1
+    falseTarget = read $ last $ splitOn " " $ lines !! 2
 
 -- Solution Logic
-part1 xs = 4
+
+getItems (Monkey items _ _ _) = items
+
+applyOperation (Monkey _ operation _ _) = operation
+
+applyTest (Monkey _ _ test _) = test
+
+getInspections (Monkey _ _ _ inspections) = inspections
+
+receiveItem (Monkey items operation test inspections) item = Monkey (items ++ [item]) operation test inspections
+
+resetItems (Monkey items operation test inspections) = Monkey [] operation test (inspections + length items)
+
+turn :: Int -> M.Map Int Monkey -> M.Map Int Monkey
+turn i monkeys =
+  M.insert i (resetItems monkey) $
+    foldl (\agg (item, to) -> M.insert to (receiveItem (agg M.! to) item) agg) monkeys throwTargets
+  where
+    monkey = monkeys M.! i
+    items = getItems monkey
+    newWorryLevels = map (\item -> applyOperation monkey item `div` 3) items
+    throwTargets = zip newWorryLevels $ map (applyTest monkey) newWorryLevels
+
+computeRound :: M.Map Int Monkey -> M.Map Int Monkey
+computeRound monkeys = foldl (flip turn) monkeys $ M.keys monkeys
+
+part1 monkeys = product $ take 2 $ reverse $ sort $ M.elems $ M.map getInspections $ iterate computeRound monkeys !! 20
