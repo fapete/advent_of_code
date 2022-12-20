8b60cea32eebb3b899001178a2c158b3d091cec4
Fabian Peternek <f.peternek@fapet.de>
2022-12-20 13:23:06 +0100

    Day 20 Part 1

--- 2022/day20/day20.hs
+++ 2022/day20/day20.hs
@@ -1,5 +1,7 @@
-import Data.List
 import System.Environment
+import Data.Sequence as Seq
+import Data.Bifunctor (second)
+import Data.Bool (bool)
 
 -- IO Scaffolding
 
@@ -15,8 +17,32 @@ solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-getInput = fmap (lines) . readFile
+getInput :: FilePath -> IO (Seq (Int, Int))
+getInput = fmap (fromList . Prelude.zip [0..] . map read . lines) . readFile
 
 -- Solution Logic
 
-part1 xs = 4
+mix :: Int -> Int -> Seq (Int, Int) -> Seq (Int, Int)
+mix pos until seq
+  | pos == until = seq
+  | origIdx /= pos = mix pos until turnCW
+  | origIdx == pos = mix (pos + 1) until $ (before |> element) >< after
+  | otherwise = error "impossible state"
+  where
+    turnCCW = (\(f, t) -> t >< f) $ Seq.splitAt 1 seq
+    turnCW = (\(i, l) -> l >< i) $ Seq.splitAt (Seq.length seq - 1) seq
+    element@(origIdx, num) = index seq pos
+    newIndex = (pos + num) `mod` (Seq.length seq - 1)
+    (before, after) = Seq.splitAt newIndex $ Seq.deleteAt pos seq
+
+unpackNums = Seq.mapWithIndex (\_ e -> snd e)
+
+turnTo0 seq@((i, num):<|xs)
+  | num == 0 = seq
+  | otherwise = turnTo0 (xs |> (i,num))
+turnTo0 xs = xs
+
+part1 xs = sum [index mixed (1000 `mod` l), index mixed (2000 `mod` l), index mixed (3000 `mod` l)]
+  where
+    l = Seq.length xs
+    mixed = unpackNums $ turnTo0 $ mix 0 l xs
