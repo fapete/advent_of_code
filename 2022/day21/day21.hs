15966c3c31e4e578acc4af6f4acf9f19bdcf7505
Fabian Peternek <f.peternek@fapet.de>
2022-12-21 08:28:28 +0100

    Day 21 part 1

--- 2022/day21/day21.hs
+++ 2022/day21/day21.hs
@@ -1,5 +1,10 @@
 import Data.List
 import System.Environment
+import Data.Bifunctor (second)
+import Data.List.Split (splitOn)
+import qualified Data.Map as M
+import Text.Read (readMaybe)
+import Data.Maybe (isJust)
 
 -- IO Scaffolding
 
@@ -15,8 +20,30 @@ solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-getInput = fmap (lines) . readFile
+getInput = fmap (M.unions . map parseMonkey . lines) . readFile
+
+data Operation = Num Int | Add String String | Sub String String | Mul String String | Div String String
+
+parseMonkey line = M.singleton monkeyname $ parseOperation operation
+  where
+    (monkeyname, operation) = second (drop 2) $ break (== ':') line
+
+parseOperation :: String -> Operation
+parseOperation op
+  | isJust (readMaybe op :: Maybe Int) = Num $ read op
+  | otherwise = case splitOn " " op of
+      [n1, "+", n2] -> Add n1 n2
+      [n1, "-", n2] -> Sub n1 n2
+      [n1, "*", n2] -> Mul n1 n2
+      [n1, "/", n2] -> Div n1 n2
+      other -> error ("Invalid operation " ++ show op)
 
 -- Solution Logic
 
-part1 xs = 4
+computeResult (Num i) monkeys = i
+computeResult (Add n n') monkeys = computeResult (monkeys M.! n) monkeys + computeResult (monkeys M.! n') monkeys
+computeResult (Sub n n') monkeys = computeResult (monkeys M.! n) monkeys - computeResult (monkeys M.! n') monkeys
+computeResult (Mul n n') monkeys = computeResult (monkeys M.! n) monkeys * computeResult (monkeys M.! n') monkeys
+computeResult (Div n n') monkeys = computeResult (monkeys M.! n) monkeys `div` computeResult (monkeys M.! n') monkeys
+
+part1 monkeys = computeResult (monkeys M.! "root") monkeys
