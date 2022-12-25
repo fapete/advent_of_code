c5e041335d41d65733ffa086f1875cb1e7813491
Fabian Peternek <f.peternek@fapet.de>
2022-12-25 10:07:47 +0100

    Day 25 Solution

---
+++ 2022/day25/README.md
@@ -0,0 +1,3 @@
+# Day 25
+
+Note: For some reason the final encoding function (`encodeSnafu`) only works with optimizations turned off (i.e., compile with `ghc -O0`).
--- 2022/day25/day25.hs
+++ 2022/day25/day25.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Maybe (fromJust)
 import System.Environment
 
 -- IO Scaffolding
@@ -16,8 +17,57 @@ solve fn = fmap fn . getInput
 
 -- Input Parsing
 
-getInput = fmap (lines) . readFile
+getInput = fmap (map parseLine . lines) . readFile
+
+parseLine :: [Char] -> [Int]
+parseLine = map parseSnafuChar
+
+parseSnafuChar '0' = 0
+parseSnafuChar '1' = 1
+parseSnafuChar '2' = 2
+parseSnafuChar '-' = -1
+parseSnafuChar '=' = -2
+parseSnafuChar _ = error "Invalid char"
 
 -- Solution Logic
 
-part1 xs = 4
+snafuToDecimal :: [Int] -> Int
+snafuToDecimal = snafuToDecimal' 0 . reverse
+
+snafuToDecimal' _ [] = 0
+snafuToDecimal' exp (x : xs) = 5 ^ exp * x + snafuToDecimal' (exp + 1) xs
+
+decimalToSnafu :: Int -> [Int]
+decimalToSnafu x = decimalToSnafu' maxExp x
+  where
+    maxExp = floor $ logBase 5 (fromIntegral x :: Float)
+
+decimalToSnafu' exp _
+  | exp < 0 = []
+decimalToSnafu' exp 0 = replicate exp 0
+decimalToSnafu' 0 1 = [1]
+decimalToSnafu' 0 2 = [2]
+decimalToSnafu' 0 3 = [1, -2]
+decimalToSnafu' 0 4 = [1, -1]
+decimalToSnafu' 0 5 = [1, 0]
+decimalToSnafu' _ (-2) = [-2]
+decimalToSnafu' _ (-1) = [-1]
+decimalToSnafu' exp x
+  | d == 0 = 0 : decimalToSnafu' (exp - 1) x
+  | m == 0 = decimalToSnafu d ++ decimalToSnafu' exp m
+  | otherwise = decimalToSnafu d ++ decimalToSnafu' (exp - 1) m
+  where
+    d = round ((fromIntegral x :: Float) / 5 ^ exp)
+    m = x - d * 5 ^ exp
+
+encodeSnafu :: [Int] -> [Char]
+encodeSnafu = map snafuChar
+
+snafuChar 1 = '1'
+snafuChar 2 = '2'
+snafuChar 0 = '0'
+snafuChar (-1) = '-'
+snafuChar (-2) = '='
+snafuChar _ = error "Incorrect char"
+
+part1 = encodeSnafu . decimalToSnafu . sum . map snafuToDecimal
