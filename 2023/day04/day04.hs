70759ea32edf03f9c2c4a52f527f42eacabed2a8
Fabian Peternek <f.peternek@fapet.de>
2023-12-04 10:12:19 +0100

    Day 4 solution

--- 2023/day04/day04.hs
+++ 2023/day04/day04.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE TupleSections #-}
+
 import Data.List
+import Data.List.Split
+import qualified Data.Set as S
+import qualified Data.Map as M
 import System.Environment
+import Data.Bifunctor (Bifunctor(bimap, second))
 
 -- IO Scaffolding
 
@@ -14,11 +20,41 @@ main = do
 solve solver parser = fmap solver . parser
 
 -- Input Parsing
+type Scratchcard = (S.Set Integer, S.Set Integer)
 
-getInput1 = fmap (lines) . readFile
+getInput1 = fmap (map parseLine . lines) . readFile
+
+parseLine :: String -> Scratchcard
+parseLine line = (S.fromList $ map read winning, S.fromList $ map read scratched)
+  where
+    numbers = drop 2 $ dropWhile (/= ':') line
+    (winning, scratched) = bimap (filter (/= "") . splitOn " ") (filter (/= "") . splitOn " " . drop 2) $ break (== '|') numbers
+
 
 -- Solution Logic
 
-part1 xs = 4
+cardPoints :: Floating a => (Scratchcard, Integer) -> a
+cardPoints ((winning, scratched), copies)
+  | numbersInBoth == 0 = 0
+  | otherwise = fromIntegral copies * (2**(fromIntegral numbersInBoth - 1))
+  where
+    numbersInBoth = S.size (S.intersection winning scratched)
 
-part2 xs = 8
+initCopies :: [Scratchcard] -> [(Scratchcard, Integer)]
+initCopies = map (, 1)
+
+numWins :: Scratchcard -> Int
+numWins (winning, scratched) = S.size (S.intersection winning scratched)
+
+makeCopies :: [(Scratchcard, Integer)] -> [(Scratchcard, Integer)]
+makeCopies [] = []
+makeCopies ((card, copies):cards) = (card, copies): makeCopies (newCopies ++ remainingCards)
+  where
+    copyCards = take (numWins card) cards
+    newCopies = map (second (+copies)) copyCards
+    remainingCards = drop (numWins card) cards
+
+
+part1 = sum . map cardPoints . initCopies
+
+part2 = sum . map snd . makeCopies . initCopies
